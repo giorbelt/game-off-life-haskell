eed159df6df40a594317e3e095ed9d12f8cf7349
Júlia Câmara <juliasgomes2002@gmail.com>
2023-10-13 10:40:20 -0300

    feat(checkAdjacentCells): Add a new function to calculate adjacent cell counts + Some adjustments

--- teste.hs
+++ teste.hs
@@ -10,6 +10,7 @@ matrixDimension = do
   rowsStr <- getLine
   putStrLn "Enter the number of columns:"
   colsStr <- getLine
+  putStrLn "\n"
   let rows = readMaybe rowsStr :: Maybe Int
       cols = readMaybe colsStr :: Maybe Int
   return $ do
@@ -17,15 +18,29 @@ matrixDimension = do
     c <- cols
     return (r, c)
 
-states :: [String]
-states = ["morto", "zumbi", "vivo"]
+states :: [Char]
+states = ['v', 'm', 'z']
 
-populateMatrix :: Int -> Int -> IO [[String]]
-populateMatrix rows cols = do
-    gen <- getStdGen
-    let getRandomState = randomRIO (0, length states - 1)
-    randomMatrix <- replicateM rows (replicateM cols getRandomState)
-    return $ map (map (states !!)) randomMatrix
+createBoard :: Int -> Int -> IO [[Char]]
+createBoard rows cols = do
+  gen <- getStdGen
+  let getRandomState = randomRIO (0, length states - 1)
+  randomMatrix <- replicateM rows (replicateM cols getRandomState)
+  return $ map (map (states !!)) randomMatrix
+
+checkAdjacentCells :: [[Char]] -> Int -> Int -> [(Char, Int)]
+checkAdjacentCells board i j = 
+    let neighbors = [
+          (x, y) | 
+          x <- [i - 1..i + 1], 
+          y <- [j - 1..j + 1], 
+          x /= i || y /= j, 
+          x >= 0, y >= 0, 
+          x < length board, y < length (head board)
+          ]
+        adjacentStates = [board !! x !! y | (x, y) <- neighbors]
+        countStates = [(state, length (filter (== state) adjacentStates)) | state <- "vmz"]
+    in countStates
 
 {-Função para contar a ocorrencia de um mesmo elemento na matriz-}
 countValues :: Eq a => [[a]] -> a -> Int
@@ -36,6 +51,12 @@ main = do
   dimensions <- matrixDimension
   case dimensions of
     Just (rows, cols) -> do
-      matrix <- populateMatrix rows cols
-      mapM_ print matrix
+      matrix <- createBoard rows cols
+      mapM_ (\row -> putStrLn (unwords [[c] | c <- row])) matrix
+
+      let i = 1
+          j = 1
+      let adjacentCells = checkAdjacentCells matrix i j 
+      putStrLn "\nTeste da função checkAdvacentCells:\nContagem de células adjacentes para celula (1,1):"
+      mapM_ (\(state, count) -> putStrLn $ state : " - " ++ show count) adjacentCells
     Nothing -> putStrLn "Invalid dimensions entered."
